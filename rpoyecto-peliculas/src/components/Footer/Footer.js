34adf0e5a609e64fc8da0d473af3055149574afb
facu-gif736 <martinezfacundo@udesa.edu.Ar>
2026-03-30 17:03:57 -0300

    Creacion footer

---
+++ rpoyecto-peliculas/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer () {
+    return (
+        <footer className="alert alert-primary mt-4 text-center">
+            <p className="mb-0">Integrante 1 | Integrante 2 | Integrante 3</p>
+        </footer>
+    );
+}
+
+export default Footer; 

f70d8b25a031f55d1616fb989a74923bef9edc1d
facu-gif736 <martinezfacundo@udesa.edu.Ar>
2026-04-03 21:57:42 -0300

    Update Card.js

--- rpoyecto-peliculas/src/components/Card/Card.js
+++ rpoyecto-peliculas/src/components/Card/Card.js
@@ -27,22 +27,20 @@ class Card extends Component {
                 <div className="cardBody">
                     <h5 className="card-title">{titulo}</h5>
                     
-                    {/* Botón para mostrar/ocultar descripción */}
+                    
                     <button className="btn btn-sm btn-info mb-2" onClick={this.toggleDescripcion}>
                         {this.state.verDescripcion ? 'Ocultar descripción' : 'Ver descripción'}
                     </button>
 
-                    {/* Si verDescripcion es true, mostramos el texto */}
+                    
                     {this.state.verDescripcion && (
                         <p className="card-text">{descripcion}</p>
                     )}
 
                     <br />
                     
-                    {/* Link para ir a la pantalla de detalle (Punto 7) */}
                     <Link to={`/detalle/${id}`} className="btn btn-primary btn-sm">Ir a detalle</Link>
                     
-                    {/* El botón de favs por ahora lo dejamos de adorno, lo hacemos andar en el Sprint 5 */}
                     <button className="btn alert-primary btn-sm ml-2">♥️</button>
                 </div>
             </article>

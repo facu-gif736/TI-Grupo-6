869291cf456841c6f3a1c6dcd9b79e4284dff2b7
facu-gif736 <martinezfacundo@udesa.edu.Ar>
2026-04-05 13:58:32 -0300

    Agregado de nombres de integrantes del grupo

--- rpoyecto-peliculas/src/components/Footer/Footer.js
+++ rpoyecto-peliculas/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer () {
     return (
         <footer className="alert alert-primary mt-4 text-center">
-            <p className="mb-0">Integrante 1 | Integrante 2 | Integrante 3</p>
+            <p className="mb-0">Facundo Martinez y Emilio Garica Oliver</p>
         </footer>
     );
 }

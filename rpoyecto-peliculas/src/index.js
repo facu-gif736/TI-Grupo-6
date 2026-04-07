e1863bd6466cbbc849aea4f36745442fcdb7ecb4
facu-gif736 <martinezfacundo@udesa.edu.Ar>
2026-04-07 13:19:33 -0300

    arreglos en index.html e index.js

--- rpoyecto-peliculas/src/index.js
+++ rpoyecto-peliculas/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.js';
 import { BrowserRouter } from 'react-router-dom';
 
 
@@ -11,6 +11,4 @@ root.render(
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
